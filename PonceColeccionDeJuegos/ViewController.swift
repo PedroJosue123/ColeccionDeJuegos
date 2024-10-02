a19a815fe4a284557173eac712be455cdfb00b35
Pedro Josue Ponce Lopez <pedro.ponce@tecsup.edu.pe>
2024-10-02 16:33:58 -0500

    Initial Commit

---
+++ PonceColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PonceColeccionDeJuegos
+//
+//  Created by Pedro Josue Ponce Lopez on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
